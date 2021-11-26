8eb2b96fd0ca95faf9f646029d83e6307d520303
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-11-26 19:49:42 +0300

    Add image model and its properties.

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
+class Image(models.Model):
+    name = models.CharField(max_length=30)
+    descrption = models.CharField()
+    location = models.ForeignKey(Location)
+    category = models.ForeignKey(Category)
+    image = models.ImageField(upload_to = 'gallery')
+    
