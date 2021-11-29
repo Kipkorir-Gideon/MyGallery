902e026e96123c5988010b8d367cc61a9d13670b
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-11-30 00:31:11 +0300

    Add location tests.

--- photos/tests.py
+++ photos/tests.py
@@ -17,7 +17,7 @@ class ImageTestClass(TestCase):
 class CategoryTestClass(TestCase):
     # Set up Method
     def setUp(self):
-        self.category = Category(category_name="favorite")
+        self.category = Category(category_name="Favorite")
         self.category.save_category()
 
     def test_instance(self):
@@ -39,3 +39,29 @@ class CategoryTestClass(TestCase):
         self.category.update_category(self.category.id, 'Food')
         changed_category = Category.objects.filter(name ='Food')
         self.assertTrue(len(changed_category) > 0)
+
+
+
+class LocationTestClass(TestCase):
+
+    def setUp(self):
+        self.location = Location(location_name = 'Karatina')
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.location,Location))
+        
+    def test_save_method(self):
+        self.location.save_location()
+        location = Location.objects.all()
+        self.assertTrue(len(location) > 0)
+
+    def test_delete_location(self):
+        self.location.delete_location()
+        location = Location.objects.all()
+        self.assertTrue(len(location)== 0) 
+       
+    def test_update_location(self):
+        self.location.save_location()
+        self.location.update_location(self.location.id, 'Karen')
+        changed_location = Location.objects.filter(name ='Karen')
+        self.assertTrue(len(changed_location) > 0) 
