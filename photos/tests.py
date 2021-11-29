2d4f74c32b2918f8c540a77683af1c2b67d8f2fd
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-11-30 00:26:47 +0300

    Add category tests.

--- photos/tests.py
+++ photos/tests.py
@@ -10,3 +10,32 @@ class ImageTestClass(TestCase):
         #Posting a new image and saving it.
         self.image = Image(name='Image1', description='An image to test the posting method.', location= self.location, category= self.category)
         self.image.save()
+
+
+
+
+class CategoryTestClass(TestCase):
+    # Set up Method
+    def setUp(self):
+        self.category = Category(category_name="favorite")
+        self.category.save_category()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.category, Category))
+
+    def test_save_method(self):
+        self.category.save_category()
+        category = Category.objects.all()
+        self.assertTrue(len(category) > 0)
+
+    def test_delete_method(self):
+        self.category.save_category()
+        self.category.delete_category()
+        category = Category.objects.all()
+        self.assertTrue(len(category) == 0)
+
+    def test_update_category(self):
+        self.category.save_category()
+        self.category.update_category(self.category.id, 'Food')
+        changed_category = Category.objects.filter(name ='Food')
+        self.assertTrue(len(changed_category) > 0)
